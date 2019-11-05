e26ca65e468f0afbe2b7287ee967a844ace81ce8
Phl3bas <Phl3bas87@gmail.com>
2019-11-05 22:30:15 +0000

    test routes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const pageOne = () => {
+  return <div>PageOne</div>;
+};
+
+const pageTwo = () => {
+  return (
+    <div>
+      PageTwo
+      <button>Click Me</button>
+    </div>
+  );
+};
 
 const App = () => {
-    return (
-        <div>
-            App
-        </div>
-    );
-}
+  return (
+    <div>
+      <BrowserRouter>
+        <Route path='/' exact component={pageOne} />
+        <Route path='/pagetwo' component={pageTwo} />
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
