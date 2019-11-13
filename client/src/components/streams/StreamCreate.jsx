49697d0303c454182fba2734497e17f65e16eaf5
Phl3bas <Phl3bas87@gmail.com>
2019-11-13 20:55:27 +0000

    added redux form and inital form inputs

--- client/src/components/streams/StreamCreate.jsx
+++ client/src/components/streams/StreamCreate.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends Component {
+  input = ({ input }) => {
+    return (
+      <div>
+        <label>{input.name}</label>
+        <input type='text' name={input.name} id='' />
+      </div>
+    );
+  };
 
-export default StreamCreate;
+  render() {
+    return (
+      <form>
+        <Field name='title' component={this.input} />
+        <Field name='description' component={this.input} />
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'steamCreate',
+})(StreamCreate);
